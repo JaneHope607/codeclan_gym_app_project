eea29ed5e8c8d233037790079b75ad56ff7e794f
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-07-10 15:20:09 +0100

    created table for classes

--- db/gym.sql
+++ db/gym.sql
@@ -2,11 +2,22 @@ DROP TABLE bookings;
 DROP TABLE classes;
 DROP TABLE members;
 
-CREATE TABLE members; (
+CREATE TABLE members (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(255),
     last_name VARCHAR (255),
     dob VARCHAR(255),
     email VARCHAR(255),
     membership_type VARCHAR(255)
+);
+
+CREATE TABLE classes (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    start_time VARCHAR(255),
+    duration INT,
+    instructor VARCHAR(255),
+    capacity INT,
+    membership_needed VARCHAR(255),
+    spaces_left INT
 );
