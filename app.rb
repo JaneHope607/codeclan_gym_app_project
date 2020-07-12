0649bdaac0bac0c6ccfb7f388c9b966ce3412e16
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-07-12 15:24:51 +0100

    created index.erb for in membres folder and added to members_controller.rb

---
+++ app.rb
@@ -0,0 +1,6 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('controllers/members_controller.rb')
+require_relative('controllers/gym_classes_controller.rb')
+require_relative('controllers/bookings_controller.rb')
+
--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -2,3 +2,8 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/member.rb')
 also_reload('../models/*')
+
+get '/members' do
+    @members = Member.find_all()
+    erb(:"members/index")
+end
