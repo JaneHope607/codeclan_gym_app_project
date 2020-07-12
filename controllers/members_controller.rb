51d59ba5f48760b0914c9d12524ff8b17ba3cda1
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-07-12 16:17:13 +0100

    create new member functionality added

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -3,13 +3,23 @@ require('sinatra/contrib/all')
 require_relative('../models/member.rb')
 also_reload('../models/*')
 
+# See all members
+
 get '/members' do
     @members = Member.find_all()
     erb(:"members/index")
 end
 
+# Show more details for member
+
 get '/members/:id' do
     @member = Member.find_by_id(params[:id].to_i)
     erb(:"members/show")
+end
+
+# Create new member (get form)
+
+get '/members/new' do
+    erb(:"members/new")
 end
 
