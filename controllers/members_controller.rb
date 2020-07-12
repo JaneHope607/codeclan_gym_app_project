e40e0de090e821227f6f09c1aec46d0b9951ebc7
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-07-12 16:49:18 +0100

    changed order of get methods to fix error on members_controller

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -10,16 +10,16 @@ get '/members' do
     erb(:"members/index")
 end
 
-# Show more details for member
-
-get '/members/:id' do
-    @member = Member.find_by_id(params[:id].to_i)
-    erb(:"members/show")
-end
-
 # Create new member (get form)
 
 get '/members/new' do
     erb(:"members/new")
+end
+
+# Show more details for member
+
+get '/members/:id' do
+    @member = Member.find_by_id(params['id'].to_i)
+    erb(:"members/show")
 end
 
