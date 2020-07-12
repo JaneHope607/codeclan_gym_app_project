155a8ca9a1a3ff640cc2cf34c1a2d4002a97fd63
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-07-12 11:27:59 +0100

    save method created for booking class

--- models/booking.rb
+++ models/booking.rb
@@ -10,5 +10,17 @@ class Booking
         @member_id = options['member_id'].to_i
     end
 
+    # Method to save booking
+
+    def save()
+        sql = "INSERT INTO bookings
+        (gymclass_id, member_id)
+        VALUES
+        ($1, $2)
+        RETURNING id"
+        values = [@gymclass_id, @member_id]
+        results = SqlRunner.run(sql, values).first
+        @id = results]['id'].to_i
+    end
 
 end
