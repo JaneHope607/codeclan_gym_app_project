b1a7366b14c58fd707e17f360ceebd384f44f498
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-07-13 14:44:12 +0100

    added gym_sessions table to sql and refactored other tables

--- db/gym_system.sql
+++ db/gym_system.sql
@@ -1,28 +1,36 @@
 DROP TABLE bookings;
+DROP TABLE gym_sessions;
 DROP TABLE gym_classes;
 DROP TABLE members;
 
 CREATE TABLE members (
     id SERIAL primary key,
-    first_name VARCHAR(255),
-    last_name VARCHAR (255),
+    first_name VARCHAR(255) not null,
+    last_name VARCHAR (255) not null,
     dob VARCHAR(255),
     email VARCHAR(255),
     number VARCHAR(15),
     date_joined VARCHAR(255),
-    membership_type VARCHAR(255)
+    membership_type VARCHAR(255) not null
 );
 
 CREATE TABLE gym_classes (
     id SERIAL primary key,
-    name VARCHAR(255),
-    duration INT,
-    instructor VARCHAR(255),
+    name VARCHAR(255) not null,
+    duration INT not null,
+    instructor VARCHAR(255)
+);
+
+CREATE TABLE gym_sessions (
+    id SERIAL primary key,
+    gymclass_id INT REFERENCES gym_classes(id),
+    day VARCHAR(255),
+    start_time VARCHAR(255),
     capacity INT
 );
 
 CREATE TABLE bookings (
     id SERIAL primary key,
-    gymclass_id INT REFERENCES gym_classes(id),
+    session_id INT REFERENCES gym_sessions(id),
     member_id INT REFERENCES members(id)
 );
