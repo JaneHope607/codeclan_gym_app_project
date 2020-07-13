ab2ed2fa645f82a0de4886823c5c6e4476947ac8
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-07-13 10:27:35 +0100

    added index functionality to show all bookings for bookings

--- controllers/bookings_controller.rb
+++ controllers/bookings_controller.rb
@@ -4,3 +4,13 @@ require_relative('../models/booking.rb')
 require_relative('../models/member.rb')
 require_relative('../models/gymclass.rb')
 also_reload('../models/*')
+
+# INDEX
+
+get '/bookings' do
+    @bookings = Booking.find_all()
+    erb(:"bookings/index")
+end
+
+# NEW
+
