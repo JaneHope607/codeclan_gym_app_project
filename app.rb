e258fa238988553d1dad83b43ba8028180e6eab9
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-09-07 23:19:33 +0100

    changes to host on heroku

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require('sinatra')
-require('sinatra/contrib/all')
+require('sinatra/contrib/all') if development?
 require_relative('controllers/members_controller.rb')
 require_relative('controllers/gym_classes_controller.rb')
 require_relative('controllers/bookings_controller.rb')
